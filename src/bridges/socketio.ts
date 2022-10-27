25033c93e34831334056d3c63f96bc31cd0ef4d5
Alexey P Nikitin <alnikitich@gmail.com>
2022-10-27 19:55:13 +0900

    renew

--- src/bridges/socketio.ts
+++ src/bridges/socketio.ts
@@ -37,7 +37,7 @@ export default class SioBridge {
     console.log(">>>>>>>>>>>>>>>>>>>>>>111")
 
     const sendSyncState = () =>  {
-      console.log(">>>>>>>>>>>>>>>>>>>>>>")
+      console.log(">>>>>>>>>>>>>>>>>>>>>>", sio_client.connected)
       const syncState = this.bds.getSyncState(); // читаем у клиента состояние для отправки на сервер
       if (sio_client.connected) sio_client.emit(`${this.nodeId}:list:state`, syncState);
     }
